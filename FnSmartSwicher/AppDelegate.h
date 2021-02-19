9a813e4f06f1a8486887d8a62a4134c44f646c83
BoHong <a60814billy@gmail.com>
2021-02-19 21:26:04 +0800

    Initial Commit

---
+++ FnSmartSwicher/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FnSmartSwicher
+//
+//  Created by Raccoon on 2021/2/19.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FnSmartSwicher/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  FnSmartSwicher
+//
+//  Created by Raccoon on 2021/2/19.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
